1faf791c50653eab6cf4a53b5562dab5b9a73bd9
giere <146406258@qq.com>
2021-04-03 17:22:26 +0800

    绘制对trigger extraction结果的分析

---
+++ visualize/plot_eval.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import numpy as np
+
+data = pickle.load(open('../train/eval_map_9.pk', 'rb'))
+data_np = np.array(data)
+
+Z = np.random.rand(6, 10)
+
+fig, ax = plt.subplots(1, 1)
+
+c = ax.pcolor(data_np)
+ax.set_title('default')
+
+
+fig.tight_layout()
+plt.show()
