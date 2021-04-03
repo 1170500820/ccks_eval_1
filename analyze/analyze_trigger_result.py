79decec19717493d0653f51add3d054003e362ae
giere <146406258@qq.com>
2021-04-03 17:23:29 +0800

    分析trigger extraction对于不同类型的结果

---
+++ analyze/analyze_trigger_result.py
@@ -0,0 +1,21 @@
+import pickle
+import numpy as np
+
+data = pickle.load(open('../train/eval_map_9.pk', 'rb'))
+val_sentences, val_types, spans, val_syntactic_features = \
+        pickle.load(open('../val_data_for_trigger_extraction.pk', 'rb'))
+a = np.array(data)
+
+a_mean = a.mean(0)
+
+type_means = {}
+for i, m in enumerate(a_mean):
+    t = val_types[i]
+    if t in type_means:
+        type_means[t].append(m)
+    else:
+        type_means[t] = [m]
+
+for key, value in type_means.items():
+    tm = np.mean(value)
+    print(key, tm)
