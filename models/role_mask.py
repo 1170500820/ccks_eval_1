bb016ead5ac77802edc85024611ab8567c28eab8
giere <146406258@qq.com>
2021-04-14 08:52:58 +0800

    加入了positive weight

--- models/role_mask.py
+++ models/role_mask.py
@@ -5,9 +5,10 @@ from settings import event_types_init, role_types, event_available_roles
 
 
 class RoleMask(nn.Module):
-    def __init__(self, rfief):
+    def __init__(self, rfief, positive_preference=3):
         super(RoleMask, self).__init__()
         self.rfief = rfief
+        self.pospref = positive_preference
         self.mask = {}
         self.weights = {}
         self.generate_mask()
@@ -33,6 +34,16 @@ class RoleMask(nn.Module):
     def return_mask(self, logits, batch_event_types):
         seq_l = logits.size(1)
         return torch.stack(list(map(lambda x: self.mask[x].repeat(seq_l, 1), batch_event_types))).cuda()
+
+    def generate_preference_mask(self, ground_truth):
+        """
+        so ground_truth is the same size as result logits, but only has 1 and 0
+        :param ground_truth: (bsz, seq_l, len(role_types))
+        :return:
+        """
+        gt_clone = ground_truth.detach().clone().float()
+        gt_clone = gt_clone * (self.pospref - 1) + 1
+        return gt_clone
 
     def return_weighted_mask(self, logits, batch_event_types):
         """
