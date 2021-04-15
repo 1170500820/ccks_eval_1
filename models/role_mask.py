bc574b2a3e2f9fb52b64d62ef8c12359b63a4891
giere <146406258@qq.com>
2021-04-15 08:12:22 +0800

    加入Focal Loss

--- models/role_mask.py
+++ models/role_mask.py
@@ -1,14 +1,15 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from settings import event_types_init, role_types, event_available_roles
+from settings import event_types_init, role_types, event_available_roles, role_alpha, role_gamma
 
 
 class RoleMask(nn.Module):
-    def __init__(self, rfief, positive_preference=3):
+    def __init__(self, rfief, alpha=role_alpha, gamma=role_gamma):
         super(RoleMask, self).__init__()
         self.rfief = rfief
-        self.pospref = positive_preference
+        self.alpha = alpha
+        self.gamma = gamma
         self.mask = {}
         self.weights = {}
         self.generate_mask()
@@ -35,14 +36,33 @@ class RoleMask(nn.Module):
         seq_l = logits.size(1)
         return torch.stack(list(map(lambda x: self.mask[x].repeat(seq_l, 1), batch_event_types))).cuda()
 
+    def return_focal_loss_mask(self, logits, ground_truth):
+        """
+
+        :param logits:
+        :param ground_truth:
+        :return:
+        """
+        logits_clone = logits.detach().clone()
+        pt = (1 - logits_clone) * ground_truth + logits_clone * (1 - ground_truth)
+        focal_weight = (self.alpha * ground_truth + (1 - self.alpha) * (1 - ground_truth)) * torch.pow(pt, self.gamma)
+        return focal_weight
+
     def generate_preference_mask(self, ground_truth):
         """
         so ground_truth is the same size as result logits, but only has 1 and 0
         :param ground_truth: (bsz, seq_l, len(role_types))
         :return:
         """
-        gt_clone = ground_truth.detach().clone().float()
-        gt_clone = gt_clone * (self.pospref - 1) + 1
+        gt_clone = ground_truth.long().tolist()
+        for b in range(ground_truth.size(0)):
+            for s in range(ground_truth.size(1)):
+                for r in range(ground_truth.size(2)):
+                    if gt_clone[b][s][r] == 0:
+                        gt_clone[b][s][r] = (1 - self.alpha)
+                    else:
+                        gt_clone[b][s][r] = self.alpha
+        gt_clone = torch.tensor(gt_clone, dtype=torch.float)
         return gt_clone
 
     def return_weighted_mask(self, logits, batch_event_types):
