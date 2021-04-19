06ace0fb7a579e03bfbfe2d9a112fbf7be99cb14
giere <146406258@qq.com>
2021-04-19 16:38:18 +0800

    改为cuda版本

--- preprocess/process_utils.py
+++ preprocess/process_utils.py
@@ -12,10 +12,10 @@ def label_smoothing(label_batch, smooth_range=label_smoothing_range):
     :param smooth_range:
     :return:
     """
-    label_copy = label_batch.clone().detach()
+    label_copy = label_batch.clone().detach().cuda()
     label_batch = label_batch * 0.9
     bsz = label_batch.size(0)
-    expanded_label = torch.cat((torch.zeros(bsz, smooth_range), label_copy, torch.zeros(bsz, smooth_range)), dim=-1)
+    expanded_label = torch.cat((torch.zeros(bsz, smooth_range).cuda(), label_copy, torch.zeros(bsz, smooth_range).cuda()), dim=-1).cuda()
     for k in range(smooth_range):
         right_label = expanded_label.T[(smooth_range - k - 1): -(smooth_range + k + 1)].T * 0.1
         left_label = expanded_label.T[(smooth_range + k + 1): -(smooth_range - k - 1) if -(smooth_range - k - 1) != 0 else None].T * 0.1
@@ -32,12 +32,12 @@ def label_smoothing_multi(labels_batch, smooth_range=label_smoothing_range):
     :param smooth_range:
     :return:
     """
-    label_copy = labels_batch.clone().detach()  # (bsz, seq_l, num_classes)
+    label_copy = labels_batch.clone().detach().cuda()  # (bsz, seq_l, num_classes)
     label_batch = labels_batch * 0.9    # (bsz, seq_l, num_classes)
     bsz, seq_l, num_classes = label_batch.size()
-    expanded_label = torch.cat((torch.zeros(bsz, num_classes, smooth_range),
-                                label_copy.permute([0, 2, 1]),
-                                torch.zeros(bsz, num_classes, smooth_range)), dim=-1)    # (bsz, num_classes, seq_l + 2 * smooth_range)
+    expanded_label = torch.cat((torch.zeros(bsz, num_classes, smooth_range).cuda(),
+                                label_copy.permute([0, 2, 1]).cuda(),
+                                torch.zeros(bsz, num_classes, smooth_range).cuda()), dim=-1).cuda()    # (bsz, num_classes, seq_l + 2 * smooth_range)
     for k in range(smooth_range):
         right_label = expanded_label.permute([2, 0, 1])[(smooth_range - k - 1): -(smooth_range + k + 1)].permute([1, 0, 2]) * 0.1   # (bsz, seq_l, num_classes)
         left_label = expanded_label.permute([2, 0, 1])[(smooth_range + k + 1): -(smooth_range - k - 1) if -(smooth_range - k - 1) != 0 else None].permute([1, 0, 2]) * 0.1  # (bsz, seq_l, num_classes)
