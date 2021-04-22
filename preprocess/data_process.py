736893b45bc392bff1c79e0dc5148b0c2420ff46
giere <146406258@qq.com>
2021-04-22 08:05:26 +0800

    训练/开发集划分相关工具

--- preprocess/data_process.py
+++ preprocess/data_process.py
@@ -12,3 +12,23 @@ def randomize(*lsts):
     random.shuffle(zipped)
     return list(zip(*zipped))
 
+
+def train_val_split(lst, split_ratio=None):
+    cnt = len(lst)
+    bound = int(cnt * split_ratio)
+    return lst[:bound], lst[bound: ]
+
+
+def multi_train_val_split(*lst, split_ratio=None):
+    """
+
+    :param lst: [lst1, lst2, ...]
+    :param split_ratio:
+    :return: train_lst1
+    """
+    trains, vals = [], []
+    for l in lst:
+        train_l, val_l = train_val_split(l, split_ratio=split_ratio)
+        trains.append(train_l)
+        vals.append(val_l)
+    return trains + vals
