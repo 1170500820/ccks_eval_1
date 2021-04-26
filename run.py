18923e1232b4c80fea6a2515136b7cd5d96554f3
giere <146406258@qq.com>
2021-04-27 00:06:53 +0800

    加入了使用LSTM层的参数，但是感觉有点冗余，网络结构不应该通过参数构建？ 加入了保存模型名字的参数；

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from train.training import train_regular_model
 from models.simple_ner import *
 from transformers import AdamW
 from settings import *
+from analyze.bad_case_analysis_utils import NerEvalRecorder
 
 
 if __name__ == '__main__':
@@ -13,9 +14,12 @@ if __name__ == '__main__':
     # train ner model in default
     # parser.add_argument('--model', type=str)
     parser.add_argument('--PLM_path', type=str)
+    parser.add_argument('--LSTM', type=bool)
 
     parser.add_argument('--do_train', type=bool)
     parser.add_argument('--do_eval', type=bool)
+
+    parser.add_argument('--save_name', type=str)
 
     parser.add_argument('--lr', type=float)
     # parser.add_argument('--bsz', type=int)
@@ -27,26 +31,32 @@ if __name__ == '__main__':
     lr = args.lr if args.lr else ner_lr
     epoch = args.epoch if args.epoch else ner_epoch
     plm_path = args.PLM_path if args.PLM_path else model_path
+    save_name = args.save_name
 
     # Step 1
     # Load model, optimizers.
-    model = BertForTokenClassification.from_pretrained(plm_path)
-    CELoss = nn.CrossEntropyLoss()
+    if args.LSTM:
+        model = NerBertLstm(model_path=model_path)
+    else:
+        model = NerBert(model_path=model_path)
+    nerloss = NerBertLoss()
 
     # Step 2
     # Prepare data
-    train_inputs, train_labels = pickle.load(open('train_input.pk')), pickle.load(open('train_labels.pk'))
-    eval_inputs, eval_labels = pickle.load(open('val_input.pk')), pickle.load(open('val_labels.pk'))
-
+    train_inputs, train_labels = pickle.load(open('train_input.pk', 'rb')), pickle.load(open('train_labels.pk', 'rb'))
+    eval_inputs, eval_labels = pickle.load(open('val_input.pk', 'rb')), pickle.load(open('val_labels.pk', 'rb'))
+    recorder = NerEvalRecorder(eval_labels)
     train_regular_model(
         lr=lr,
         epoch=epoch,
-        save_freq=None,
-        eval_freq=120,
+        save_freq=1,
+        eval_freq=250,
         model=model,
         train_inputs=train_inputs,
         train_labels=train_labels,
         val_inputs=eval_inputs,
         val_labels=eval_labels,
-        recorder=None
+        loss_function=nerloss,
+        recorder=recorder,
+        save_name=save_name
     )
